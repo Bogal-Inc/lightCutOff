7a382e79094b0262646de6524a7998a0634daf4e
willy <willkoua@gmail.com>
2020-05-20 16:29:45 +0100

    put the form in popup marker map

--- src/app/main/map/map-view/map-view.component.ts
+++ src/app/main/map/map-view/map-view.component.ts
@@ -2,7 +2,6 @@ import { IMarker } from './../../../core/models/marker.model';
 import { Component, OnInit } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
@@ -10,29 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapViewComponent implements OnInit {
   currentPosition: IMarker;
-  currentPositionMap: IMarker;
   zoom: number;
   selectedCurrentMarker = false;
+  markers = [
+    // These are all just random coordinates from https://www.random.org/geographic-coordinates/
+    { lat: 4.0520564, lng: 9.7618687, alpha: 0.5 },
+    { lat: 4.0530564, lng: 9.7628687, alpha: 0.5 },
+    { lat: 4.0540564, lng: 9.7638687, alpha: 0.5 },
+    { lat: 4.0550564, lng: 9.7648687, alpha: 0.5 },
+    { lat: 4.0560564, lng: 9.7658687, alpha: 0.5 },
+    { lat: 4.0570564, lng: 9.7668687, alpha: 0.5 }
+  ];
 
-
-  constructor(
-
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.zoom = 15;
+    this.zoom = 13;
     navigator.geolocation.getCurrentPosition( position => {
       this.currentPosition = {
         latitude: +position.coords.latitude,
         longitude: +position.coords.longitude,
         alpha: 1
       };
-      this.currentPositionMap = {
-        latitude: +position.coords.latitude,
-        longitude: +position.coords.longitude,
-        alpha: 1
-      };
     });
+  }
+
+  selectMarker(event) {
+    console.log("hello world")
   }
 
   currentMarker(latitude: number, longitude: number)  {
@@ -44,11 +47,10 @@ export class MapViewComponent implements OnInit {
   }
 
   selectCurrentMarker(event) {
-    if(this.selectedCurrentMarker) {
+    if (this.selectedCurrentMarker) {
       this.selectedCurrentMarker = false;
     } else {
       this.selectedCurrentMarker = true;
     }
-
   }
 }
