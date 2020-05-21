ba5f6c649e3d0595e621803e5e538c7875ac4a27
willy <willkoua@gmail.com>
2020-05-21 22:15:22 +0100

    add clusterer

--- src/app/main/map/map-view/map-view.component.ts
+++ src/app/main/map/map-view/map-view.component.ts
@@ -1,7 +1,7 @@
+import MarkerClusterer, { MarkerClustererOptions } from '@google/markerclusterer'
 import { SelectCurrentMarkerComponent } from './../components/select-current-marker/select-current-marker.component';
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
-
 
 @Component({
   selector: 'app-map-view',
@@ -20,20 +20,19 @@ export class MapViewComponent implements OnInit, AfterViewInit {
   markerCurrentPosition: google.maps.Marker;
   markers = [
     // These are all just random coordinates from https://www.random.org/geographic-coordinates/
-    { lat: 4.0520564, lng: 9.7618687, alpha: 0.5 },
-    { lat: 4.0530564, lng: 9.7628687, alpha: 0.5 },
-    { lat: 4.0540564, lng: 9.7638687, alpha: 0.5 },
-    { lat: 4.0550564, lng: 9.7648687, alpha: 0.5 },
-    { lat: 4.0560564, lng: 9.7658687, alpha: 0.5 },
-    { lat: 4.0570564, lng: 9.7668687, alpha: 0.5 }
+    { lat: 4.0520564, lng: 9.7618687 },
+    { lat: 4.0530564, lng: 9.7628687 },
+    { lat: 4.0540564, lng: 9.7638687 },
+    { lat: 4.0550564, lng: 9.7648687 },
+    { lat: 4.0560564, lng: 9.7658687 },
+    { lat: 4.0570564, lng: 9.7668687 }
   ];
 
   constructor(
     private mapsApiLoader: MapsAPILoader,
   ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
     this.mapInitializer();
@@ -46,34 +45,57 @@ export class MapViewComponent implements OnInit, AfterViewInit {
         const lat = +position.coords.latitude;
         this.coordinates = new google.maps.LatLng(lat, lng);
 
-        this.mapOptions = {
-          center: this.coordinates,
-          zoom: 12,
-          backgroundColor: '#eaeaea',
-          mapTypeControl: false,
-          streetViewControl: false
-        };
+        this.initMap();
 
-        this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
-        this.addMarkerToMap({
+        this.addCurrentMarkerToMap({
           position: this.coordinates,
-          map: this.map,
           label: 'Votre position'
         });
 
-        const infoWindow = new google.maps.InfoWindow({
-          content: this.formLightCutOff.nativeElement
-        });
+        // const markerCurrentPosition = new MarkerClusterer(
+        //   this.map,
+        //   this.markers,
+        //   {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'}
+        // );
 
-        this.markerCurrentPosition.addListener('click', function() {
-          infoWindow.open(this.getMap(), this);
-        });
+        this.generateMarkerExple();
       });
     });
   }
 
-  private addMarkerToMap(markerOption: google.maps.MarkerOptions) {
+  private initMap() {
+    this.mapOptions = {
+      center: this.coordinates,
+      zoom: 12,
+      backgroundColor: '#eaeaea',
+      mapTypeControl: false,
+      streetViewControl: false
+    };
+
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+  }
+
+  private addCurrentMarkerToMap(markerOption: google.maps.MarkerOptions) {
     this.markerCurrentPosition = new google.maps.Marker(markerOption);
     this.markerCurrentPosition.setMap(this.map);
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: this.formLightCutOff.nativeElement
+    });
+
+    this.markerCurrentPosition.addListener('click', function() {
+      infoWindow.open(this.getMap(), this);
+    });
+  }
+
+  private generateMarkerExple() {
+    for (let i = 0; i < this.markers.length; i++) {
+      let coords = new google.maps.LatLng(this.markers[i].lat, this.markers[i].lng);
+      let marker = new google.maps.Marker({
+        position: coords,
+        opacity: 0.5
+      });
+      marker.setMap(this.map);
+    }
   }
 }
