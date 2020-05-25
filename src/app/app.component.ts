c7a16f449acf4cb288b7393627d6fe0879ce9f8d
willy <willkoua@gmail.com>
2020-05-25 23:41:13 +0100

    fix config to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../environments/environment';
-import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-root',
@@ -10,19 +9,7 @@ import * as firebase from 'firebase/app';
 export class AppComponent implements OnInit {
   title = 'lightcutoff';
 
-  constructor() {
-    const firebaseConfig = {
-      apiKey: environment.firebase.apiKey,
-      authDomain: environment.firebase.authDomain,
-      databaseURL: environment.firebase.databaseURL,
-      projectId: environment.firebase.projectId,
-      storageBucket: environment.firebase.storageBucket,
-      messagingSenderId: environment.firebase.messagingSenderId,
-      appId: environment.firebase.appId,
-      measurementId: environment.firebase.measurementId
-    };
-    firebase.initializeApp(firebaseConfig);
-  }
+  constructor() { }
 
   ngOnInit() {}
 }
