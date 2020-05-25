bc58afa403ebe3ccb48cb31df651323678a8a3c1
willy <willkoua@gmail.com>
2020-05-25 08:21:20 +0100

    configure firebase

--- set-env.ts
+++ set-env.ts
@@ -11,7 +11,17 @@ require('dotenv').config();
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
   production: '${process.env.PRODUCTION}',
-  GOOGLE_MAPS_API_KEY: '${process.env.GOOGLE_MAPS_API_KEY}',
+  googleMapsApiKey: '${process.env.GOOGLE_MAPS_API_KEY}',
+  firebase: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}',
+    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}'
+  }
 };
 `;
 
