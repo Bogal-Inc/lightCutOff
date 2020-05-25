c209b49934929ab9c2ece90aaa03921be26e261c
willy <willkoua@gmail.com>
2020-05-25 11:49:54 +0100

    add model for report

---
+++ src/app/core/models/report.model.ts
@@ -0,0 +1,10 @@
+export class Report {
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+  reportedAt: string;
+  restoredAt: string;
+  position: google.maps.LatLng;
+  acceptLangage: string;
+  userAgent: string;
+}
