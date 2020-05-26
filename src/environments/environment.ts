ed9c02c69f1640109cbcb3275acb98f0e940c947
willy <willkoua@gmail.com>
2020-05-26 18:19:52 +0100

    update environement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,17 @@
 
 export const environment = {
   production: false,
-  googleMapsApiKey: '',
-  firebase: {}
+  googleMapsApiKey: 'YOUR_GOOGLE_MAP_API_KEY',
+  firebase: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    databaseURL: 'YOUR_DATABASE_URL',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    measurementId: 'YOUR_MEASUREMENT_ID',
+    appId: 'YOUR_APP_ID'
+  }
 };
 
 /*
