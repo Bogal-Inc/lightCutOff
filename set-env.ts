2f2ee714d263a79f858d380dad94d74ccd6087b6
willy <willkoua@gmail.com>
2020-05-27 12:07:18 +0100

    update environment path

--- set-env.ts
+++ set-env.ts
@@ -31,13 +31,13 @@ const envConfigFile = `export const environment = {
 };
 `;
 
-console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+console.log(colors.magenta('The file `environment.prod.ts` will be written with the following content: \n'));
 console.log(colors.grey(envConfigFile));
 
 writeFile(targetPath, envConfigFile, (err) => {
    if (err) {
        throw console.error(err);
    } else {
-       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+       console.log(colors.magenta(`Angular environment.prod.ts file generated correctly at ${targetPath} \n`));
    }
 });
