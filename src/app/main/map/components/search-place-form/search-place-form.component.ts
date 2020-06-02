9476828c4797574627d9c2d258a81f537def361b
willy <willkoua@gmail.com>
2020-06-03 00:09:25 +0100

    init search place form component

---
+++ src/app/main/map/components/search-place-form/search-place-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-place-form',
+  templateUrl: './search-place-form.component.html',
+  styleUrls: ['./search-place-form.component.scss']
+})
+export class SearchPlaceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
