4b67b3c90e6c21d0bc5e516fa7764b4a7e478931
willy <willkoua@gmail.com>
2020-06-03 13:46:31 +0100

    add research place fonctionnality

--- src/app/main/map/components/search-place-form/search-place-form.component.ts
+++ src/app/main/map/components/search-place-form/search-place-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-place-form',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-place-form.component.scss']
 })
 export class SearchPlaceFormComponent implements OnInit {
+  @Output() searchPlaceSubmit: EventEmitter<any> = new EventEmitter<any>();
+  private submitted = false;
+  searchPlaceForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.initReportForm();
+  }
+
+  private initReportForm() {
+    this.searchPlaceForm = this.formBuilder.group({
+      query: ['', [Validators.required]],
+    });
+  }
+
+  get f() { return this.searchPlaceForm.controls; }
+
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.searchPlaceForm.invalid) {
+      return;
+    }
+
+    const request = this.searchPlaceForm.value;
+    this.searchPlaceSubmit.emit(request);
   }
 
 }
