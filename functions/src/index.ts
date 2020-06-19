113f0908e1cb11c0609f97fcca8605b411594682
willy <willkoua@gmail.com>
2020-06-19 20:59:19 +0100

    add a function for support email

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp(functions.config().firebase);
+admin.firestore().settings({ timestampsInSnapshots: true });
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export * from './callable';
