ec065d42be42e5fce83cec4e270970c9689025f3
willy <willkoua@gmail.com>
2020-06-22 13:06:20 +0100

    fix test ReportService

--- src/app/core/services/report.service.spec.ts
+++ src/app/core/services/report.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReportService } from './report.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
-describe('MarkerService', () => {
+describe('ReportService', () => {
   let service: ReportService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+      ],
+    });
     service = TestBed.inject(ReportService);
   });
 
