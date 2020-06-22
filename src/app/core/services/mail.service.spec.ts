441e9a3a32cc2d63a6e96a8e09c5288c74d8da32
willy <willkoua@gmail.com>
2020-06-22 13:17:37 +0100

    fix test MailService

--- src/app/core/services/mail.service.spec.ts
+++ src/app/core/services/mail.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { MailService } from './mail.service';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 describe('MailService', () => {
   let service: MailService;
@@ -10,6 +13,8 @@ describe('MailService', () => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
       ],
     });
     service = TestBed.inject(MailService);
