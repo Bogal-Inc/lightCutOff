144e4cf9e0a3b1e194d74750b860ad7741a9f166
willy <willkoua@gmail.com>
2020-06-22 13:02:27 +0100

    fix test searchPlaceComponent

--- src/app/main/map/components/search-place-form/search-place-form.component.spec.ts
+++ src/app/main/map/components/search-place-form/search-place-form.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchPlaceFormComponent } from './search-place-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SearchPlaceFormComponent', () => {
   let component: SearchPlaceFormComponent;
@@ -8,7 +10,12 @@ describe('SearchPlaceFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchPlaceFormComponent ]
+      declarations: [ SearchPlaceFormComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
