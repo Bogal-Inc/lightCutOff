db08f81fed5327990d1b8b20f4e9c2fa3c1c7887
willy <willkoua@gmail.com>
2020-06-22 13:13:09 +0100

    fix test MapViewComponent

--- src/app/main/map/map-view/map-view.component.spec.ts
+++ src/app/main/map/map-view/map-view.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapViewComponent } from './map-view.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { ToastrModule } from 'ngx-toastr';
+import { AgmCoreModule } from '@agm/core';
 
 describe('MapViewComponent', () => {
   let component: MapViewComponent;
@@ -8,7 +13,21 @@ describe('MapViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapViewComponent ]
+      declarations: [ MapViewComponent ],
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        ToastrModule.forRoot({
+          timeOut: 10000,
+          progressBar: true
+        }),
+        AgmCoreModule.forRoot({
+          apiKey: environment.googleMapsApiKey,
+          region: 'CM',
+          language: 'fr',
+          libraries: ['places']
+        }),
+      ],
     })
     .compileComponents();
   }));
