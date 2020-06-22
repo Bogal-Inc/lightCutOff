b0e3612ab290ab7622bf033dad63aaa91ac635ea
willy <willkoua@gmail.com>
2020-06-22 01:51:25 +0100

    fix test

--- src/app/core/pipes/timestamp.pipe.spec.ts
+++ src/app/core/pipes/timestamp.pipe.spec.ts
@@ -2,7 +2,7 @@ import { TimestampPipe } from './timestamp.pipe';
 
 describe('TimestampPipe', () => {
   it('create an instance', () => {
-    const pipe = new TimestampPipe();
+    const pipe = new TimestampPipe('fr-FR');
     expect(pipe).toBeTruthy();
   });
 });
