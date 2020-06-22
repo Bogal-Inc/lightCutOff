db440006ecbf3a6d8c286527aaa9c7a66625d53f
willy <willkoua@gmail.com>
2020-06-22 13:15:41 +0100

    fix test BaseService

--- src/app/core/services/base.service.spec.ts
+++ src/app/core/services/base.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BaseService } from './base.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 describe('BaseService', () => {
   let service: BaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+      ],
+    });
     service = TestBed.inject(BaseService);
   });
 
