816609ff2ee6004b0246750d0cf4794154a9fd79
willy <willkoua@gmail.com>
2020-06-25 19:53:09 +0100

    add text in about us

--- src/app/main/general/about-us/about-us.component.ts
+++ src/app/main/general/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Const } from 'src/environments/const';
 
 @Component({
   selector: 'app-about-us',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+  projectTitle = Const.app.title
 
   constructor() { }
 
