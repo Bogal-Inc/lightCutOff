9272210af52e7c6147a5d72c1b16dc2371883b6d
willy kouagnia <willkoua@gmail.com>
2020-07-09 23:05:50 +0100

    add viewReports component

--- src/app/main/report/report-routing.module.ts
+++ src/app/main/report/report-routing.module.ts
@@ -1,12 +1,17 @@
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ViewReportsComponent} from './view-reports/view-reports.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+  },
+  {
+    path: 'view',
+    component: ViewReportsComponent,
   },
 ];
 
