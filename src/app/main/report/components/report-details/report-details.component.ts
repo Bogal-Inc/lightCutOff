3054b6059fbb83cbce676129773072b67deb236c
willy kouagnia <willkoua@gmail.com>
2020-07-14 00:05:24 +0100

    fix reportStatus indefined

--- src/app/main/report/components/report-details/report-details.component.ts
+++ src/app/main/report/components/report-details/report-details.component.ts
@@ -51,7 +51,7 @@ export class ReportDetailsComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.report?.currentValue) {
       this.report = changes.report.currentValue[0];
-      this.reportStatus = (this.report.status === ReportSatus.CUT);
+      this.reportStatus = (this.report?.status === ReportSatus.CUT);
     }
   }
 
