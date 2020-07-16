faee743bb781813cce7dcb0e81ca4158ed3c2334
willy kouagnia <willkoua@gmail.com>
2020-07-16 22:41:10 +0100

    update report path in the .gitignore

---
+++ src/app/main/report/report.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {TimestampPipe} from '@Pipes/timestamp.pipe';
+import { ReportDatatableComponent } from './components/report-datatable/report-datatable.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { ReportRoutingModule } from './report-routing.module';
+import {ReportDetailsComponent} from './components/report-details/report-details.component';
+import {ReportDetailsMobileComponent} from './report-details-mobile/report-details-mobile.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import {ReportListComponent} from './report-list/report-list.component';
+
+
+@NgModule({
+  declarations: [
+    ReportDetailsMobileComponent,
+    ReportDetailsComponent,
+    ReportListComponent,
+    ReportDatatableComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReportRoutingModule,
+    SharedModule,
+    AgGridModule.withComponents([]),
+  ],
+  exports: [
+    ReportDatatableComponent
+  ],
+  providers: [TimestampPipe]
+})
+export class ReportModule { }
