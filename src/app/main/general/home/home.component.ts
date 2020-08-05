d6418e909097340d36a87eca79348c3a1a9c4514
willy kouagnia <willkoua@gmail.com>
2020-08-05 15:00:47 +0100

    add section header in home page

--- src/app/main/general/home/home.component.ts
+++ src/app/main/general/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Logger} from '@Services/logger.service';
-
+import {faPlay} from '@fortawesome/free-solid-svg-icons';
 
 const log = new Logger('home.component');
 
@@ -10,11 +10,18 @@ const log = new Logger('home.component');
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  faPlay = faPlay;
+  showVideo = false;
 
   constructor() { }
 
   ngOnInit(): void {
     log.debug('init');
   }
+
+  toggleVideo() {
+    this.showVideo = (!this.showVideo);
+  }
+
 
 }
