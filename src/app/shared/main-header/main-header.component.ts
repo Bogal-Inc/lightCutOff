bb435b4f447bee4dc28dfdb782d2acc8d8afea15
willy kouagnia <willkoua@gmail.com>
2020-08-05 15:42:17 +0100

    add fixed-top option in header

--- src/app/shared/main-header/main-header.component.ts
+++ src/app/shared/main-header/main-header.component.ts
@@ -3,6 +3,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import { Const } from 'src/environments/const';
 import { Logger } from '@Services/logger.service';
 import {ConnectionService} from '@Services/connection.service';
+import {Router} from '@angular/router';
 
 const log = new Logger('main-header.component');
 
@@ -14,15 +15,26 @@ const log = new Logger('main-header.component');
 export class MainHeaderComponent implements OnInit {
   appTitle = Const.app.title;
   online: boolean;
+  fixedTop = false;
 
   constructor(
     private authService: AuthService,
     private connectionService: ConnectionService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     log.debug('init');
+
     // check connection status
+    this.isOnlineStatus();
+
+    // fixed header or not
+    const url = this.router.url;
+    this.fixedTop = (url.split('/')[1] === '');
+  }
+
+  isOnlineStatus() {
     this.connectionService.start();
     this.connectionService.behaviorSubjectObservable$.subscribe(online => {
       const logMessage = (online) ? 'app online' : 'app off line';
