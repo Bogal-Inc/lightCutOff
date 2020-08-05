3b0963c6838171100635f9a11861ee371218cfb2
willy kouagnia <willkoua@gmail.com>
2020-08-05 21:01:07 +0100

    update section 1 home page

--- src/app/main/general/home/home.component.ts
+++ src/app/main/general/home/home.component.ts
@@ -1,27 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {Logger} from '@Services/logger.service';
 import {faPlay} from '@fortawesome/free-solid-svg-icons';
+import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
 
 const log = new Logger('home.component');
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class HomeComponent implements OnInit {
   faPlay = faPlay;
-  showVideo = false;
+  closeResult = '';
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    config: NgbModalConfig
+  ) {
+    config.centered = true;
+    config.size = 'lg';
+  }
 
   ngOnInit(): void {
     log.debug('init');
   }
 
-  toggleVideo() {
-    this.showVideo = (!this.showVideo);
+  openModal(content) {
+    this.modalService.open(content);
   }
-
-
 }
