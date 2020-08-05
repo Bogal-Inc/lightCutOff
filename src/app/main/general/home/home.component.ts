08e893e855621fb08d8434e2611cd66c8e772838
willy kouagnia <willkoua@gmail.com>
2020-08-05 15:42:33 +0100

    add about section in home page

--- src/app/main/general/home/home.component.ts
+++ src/app/main/general/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Logger} from '@Services/logger.service';
 import {faPlay} from '@fortawesome/free-solid-svg-icons';
 
