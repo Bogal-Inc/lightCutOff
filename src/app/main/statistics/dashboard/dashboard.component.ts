990561ade1ea37706e38ed39055725525614d95c
willy kouagnia <willkoua@gmail.com>
2020-10-02 16:44:51 +0100

    fix design chart-week

--- src/app/main/statistics/dashboard/dashboard.component.ts
+++ src/app/main/statistics/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ReportService} from '@Services/report.service';
-import {Report} from '@Models/report.model';
-import { faChartLine, faChartPie, faChartArea } from '@fortawesome/free-solid-svg-icons';
+import {Report, ReportSatus} from '@Models/report.model';
+import {faChartArea, faChartLine, faChartPie} from '@fortawesome/free-solid-svg-icons';
 import {TranslateService} from '@ngx-translate/core';
 import {Logger} from '@Services/logger.service';
 import {Const} from '../../../../environments/const';
 import {MetaService} from '@Services/meta.service';
 import {AngularFireAnalytics} from '@angular/fire/analytics';
+import {Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
 
 const log = new Logger('dashboard.component');
 
@@ -16,8 +18,11 @@ const log = new Logger('dashboard.component');
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  reports$: Observable<Report[]>;
   card2ReportsCurrentYear: Report[];
-  cardReportsCurrentMonth: Report[];
+  reportsCurrentMonth: Report[];
+  reportsYearClose: Report[];
+  reportsMonthClose: Report[];
   reports: Report[];
   reportsCurrentYear: Report[];
   cards: any;
@@ -38,49 +43,71 @@ export class DashboardComponent implements OnInit {
     this.initCardDashbord();
   }
 
-  private async initCardDashbord() {
-    const now = new Date();
-
-    this.reportService.getReports({
+  private initCardDashbord() {
+    this.reports$ = this.reportService.getReports({
       isDeleted: false,
-      datestart: new Date(now.getFullYear() + '/1/1')
-    }).subscribe(
-      reports => {
-        const currentDate = new Date();
-        this.reports = reports;
+      limit: 10
+    }).pipe(
+      tap((data) => {
+        this.reports = data;
 
-        this.reportsCurrentYear = this.reports.filter(
-          (report) => (report.reportedAt.toDate().getFullYear() === currentDate.getFullYear())
-        );
-
-        this.cardReportsCurrentMonth = this.reports.filter(
-          (report) => (report.reportedAt.toDate().getMonth() === currentDate.getMonth())
-        );
-
-        this.initDashboardCards(this.reportsCurrentYear, this.cardReportsCurrentMonth);
-      }
+        this.initReportsCollection();
+        this.initDashboardCards();
+      })
     );
   }
 
-  private initDashboardCards(reportsCurrentYear, reportsCurrentMonth) {
+  private initReportsCollection() {
+    const now = new Date();
+
+    this.reportsCurrentYear = this.reports.filter(
+      report => report.reportedAt.toDate().getFullYear() === now.getFullYear()
+    );
+
+    this.reportsCurrentMonth = this.reportsCurrentYear.filter(
+      report => report.reportedAt.toDate().getMonth() === now.getMonth()
+    );
+
+    this.reportsYearClose = this.reportsCurrentYear.filter(
+      report => report.status === ReportSatus.CUT
+    );
+
+    this.reportsMonthClose = this.reportsCurrentMonth.filter(
+      report => report.status === ReportSatus.CUT
+    );
+  }
+
+  private initDashboardCards() {
     this.cards = [
       {
         title: this.translateService.instant('statistics.dashboard.card_reports_all_title'),
         body: this.reports.length,
         icon: faChartLine,
-        style: 'bg-success'
+        style: 'border-left-success'
       },
       {
         title: this.translateService.instant('statistics.dashboard.card_reports_current_year_title'),
-        body: reportsCurrentYear.length,
+        body: this.reportsCurrentYear.length,
         icon: faChartPie,
-        style: 'bg-danger'
+        style: 'border-left-danger'
       },
       {
         title: this.translateService.instant('statistics.dashboard.card_reports_current_month_title'),
-        body: reportsCurrentMonth.length,
+        body: this.reportsCurrentMonth.length,
         icon: faChartArea,
-        style: 'bg-warning'
+        style: 'border-left-warning'
+      },
+      {
+        title: 'rapport annuelle clos',
+        body: this.reportsYearClose.length,
+        icon: faChartArea,
+        style: 'border-left-info'
+      },
+      {
+        title: 'rapport mensuel clos',
+        body: this.reportsMonthClose.length,
+        icon: faChartArea,
+        style: 'border-left-secondary'
       }
     ];
   }
