43252642046e4f1fca1eba4bf3c8ec7f4630d4f5
willy kouagnia <willkoua@gmail.com>
2020-10-24 10:32:55 +0100

    add style to element select

--- src/app/main/map/components/map-history-marker/map-history-marker.component.ts
+++ src/app/main/map/components/map-history-marker/map-history-marker.component.ts
@@ -1,13 +1,13 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {Report, ReportSatus} from '@Models/report.model';
 import {isMobile} from '@Helpers/mobile-confirm.helper';
 import {durationToString, getDuration} from '@Helpers/date.helper';
 import {faAngleRight, faCircle, faUser} from '@fortawesome/free-solid-svg-icons';
-import {AuthService} from '../../../../core/services-firebase/auth.service';
+import {AuthService} from '../../../../core/services-firebase';
 import {Logger} from '@Services/logger.service';
 import {AngularFireAnalytics} from '@angular/fire/analytics';
-import {Const} from '../../../../../environments/const';
 import {environment} from '../../../../../environments/environment';
+import {appendHtmlElementToHead} from '@angular/cdk/schematics';
 
 const log = new Logger('map-history-marker.component');
 
@@ -26,11 +26,11 @@ export class MapHistoryMarkerComponent implements OnInit, OnChanges {
   readonly moduleConfig = environment.app.modules.mapMenu;
   reportsMonthly: Report[];
   reportsNotClosed: Report[];
+  reportsDayNotClosed: Report[];
+  reportsDay: Report[];
   now: Date;
   showHistory = false;
   active = 1;
-  reportsDay: Report[];
-  reportsDayNotClosed: Report[];
 
   constructor(
     private authService: AuthService,
@@ -43,35 +43,39 @@ export class MapHistoryMarkerComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const reportsTmp = changes.reports.currentValue;
+    const reportsCurrent = changes.reports.currentValue;
+    const reportsPrevious = changes.reports.previousValue;
 
-    if (reportsTmp) {
+    if (reportsCurrent) {
       this.showHistory = true;
-      this.reportsMonthly = reportsTmp.filter(
+
+      this.reportsMonthly = reportsCurrent.filter(
         report => {
           return report.reportedAt.toDate().getMonth() === this.now.getMonth();
         }
       );
+
+      // the sort is automatic
+      this.reportsMonthly.sort(
+        (a: any, b: any) => {
+          const aDate = a._updatedAt.seconds;
+          const bDate = b._updatedAt.seconds;
+
+          if (aDate < bDate) {
+            return 1;
+          } else if (aDate > bDate) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+
       this.reportsDay = this.reportsMonthly.filter(
         report => {
           return report.reportedAt.toDate().getDate() === this.now.getDate();
         }
       );
-      // the sort is automatic
-      // this.reportsMonthly.sort(
-      //   (a: any, b: any) => {
-      //     const aDate = a._updatedAt.seconds;
-      //     const bDate = b._updatedAt.seconds;
-      //
-      //     if (aDate < bDate) {
-      //       return 1;
-      //     } else if (aDate > bDate) {
-      //       return -1;
-      //     } else {
-      //       return 0;
-      //     }
-      //   }
-      // );
+
       this.reportsNotClosed = this.reportsMonthly.filter(
         report => report.status === ReportSatus.CUT
       );
@@ -86,13 +90,14 @@ export class MapHistoryMarkerComponent implements OnInit, OnChanges {
     return report.status === ReportSatus.CUT_COMPLETED;
   }
 
-  moveToMarker(report) {
+  moveToMarker(report: Report, index: number) {
     log.debug('move to marker');
     this.analytics.logEvent('select_content', {
       report,
       where: 'map-history'
     });
 
+    this.changeStyleOnElementHistory(index);
     this.goToMarker.emit(report);
   }
 
@@ -107,5 +112,15 @@ export class MapHistoryMarkerComponent implements OnInit, OnChanges {
 
   isOwner(report: Report) {
     return this.authService.getUser().id === report._createdBy.id;
+  }
+
+  private changeStyleOnElementHistory(index: number) {
+    const elements = document.querySelectorAll('.report__nav__item__content__elt');
+    elements.forEach(
+      element => element.className = 'list-group-item list-group-item-action report__nav__item__content__elt'
+    );
+
+    const elementClicked = document.querySelector('#report-item-' + index);
+    elementClicked.className = 'list-group-item list-group-item-action report__nav__item__content__elt report__nav__item__content__elt-selected';
   }
 }
