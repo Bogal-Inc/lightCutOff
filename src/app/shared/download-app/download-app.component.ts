e3ce77a5761d7822795e63116e454269077ea461
willy kouagnia <willkoua@gmail.com>
2020-11-22 23:14:05 +0100

    add active download component

--- src/app/shared/download-app/download-app.component.ts
+++ src/app/shared/download-app/download-app.component.ts
@@ -14,7 +14,7 @@ const log = new Logger('loading.component');
 export class DownloadAppComponent implements OnInit {
   readonly appTitle = Const.app.title;
   deferredPrompt: any;
-  showDownload = false;
+  showDownload = true;
 
   @HostListener('window:beforeinstallprompt', ['$event'])
   onbeforeinstallprompt(e) {
