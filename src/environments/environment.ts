1e420da743ddb98f9278297c807d2c592ee73b78
willy kouagnia <willkoua@gmail.com>
2020-11-25 11:21:48 +0100

    add new icons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,24 @@
 export const environment = {
   production: true,
-  environment: 'prod',
-  domain: 'https://www.lightcutoff.com',
-  googleMapsApiKey: 'AIzaSyAgKrc6r4KHEcuiJ11qY2_H2ID2dD6n1cI',
+  environment: 'staging',
+  googleMapsApiKey: 'googleMapsApiKey',
+  domain: 'localhost',
   app: {
     modules: {
       ownerReport: true,
-      mapMenu: false,
+      mapMenu: true,
       mapFilter: false,
       mapSearch: true
     }
   },
   firebase: {
-    apiKey: 'AIzaSyALW_XHt5fh0nVNbz00XF6HnhDxouDLVy8',
-    authDomain: 'lightcutoff.firebaseapp.com',
-    databaseURL: 'https://lightcutoff.firebaseio.com',
-    messagingSenderId: '735072351565',
-    appId: '1:735072351565:web:7ae540ed2c96fcfe717304',
-    measurementId: 'G-TQMFJW8J8B',
-    projectId: 'lightcutoff',
-    storageBucket: 'lightcutoff.appspot.com'
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseURL',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId',
+    measurementId: 'measurementId',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket'
   }
 };
