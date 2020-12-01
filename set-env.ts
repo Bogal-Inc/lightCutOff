2bc487780a783fa21b6cb47d0f07b1f16f604c41
willy kouagnia <willkoua@gmail.com>
2020-12-02 00:14:42 +0100

    refactor set-env

--- set-env.ts
+++ set-env.ts
@@ -14,83 +14,12 @@ let envConfigFile;
 
 if (environment === 'prod') {
   targetPath += 'environment.prod.ts';
-  envConfigFile = `export const environment = {
-    production: ${process.env.PRODUCTION},
-    environment: '${process.env.ENVIRONMENT}',
-    domain: '${process.env.DOMAIN_PROD}',
-    googleMapsApiKey: '${process.env.GOOGLE_MAPS_API_KEY}',
-    app: {
-      modules: {
-        ownerReport: true,
-        mapMenu: false,
-        mapFilter: false,
-        mapSearch: true
-      }
-    },
-    firebase: {
-      apiKey: '${process.env.FIREBASE_API_KEY}',
-      authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-      databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
-      messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-      appId: '${process.env.FIREBASE_APP_ID}',
-      measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
-      projectId: '${process.env.FIREBASE_PROJECT_ID}',
-      storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}'
-    }
-  };`;
 } else if (environment === 'staging')  {
   targetPath += 'environment.staging.ts';
-  envConfigFile = `export const environment = {
-    production: ${process.env.PRODUCTION},
-    environment: '${process.env.ENVIRONMENT}',
-    domain: '${process.env.DOMAIN_PROD}',
-    googleMapsApiKey: '${process.env.GOOGLE_MAPS_API_KEY}',
-    app: {
-      modules: {
-        ownerReport: true,
-        mapMenu: true,
-        mapFilter: true,
-        mapSearch: true
-      }
-    },
-    firebase: {
-      apiKey: '${process.env.FIREBASE_API_KEY_DEV}',
-      authDomain: '${process.env.FIREBASE_AUTH_DOMAIN_DEV}',
-      databaseURL: '${process.env.FIREBASE_DATABASE_URL_DEV}',
-      messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID_DEV}',
-      appId: '${process.env.FIREBASE_APP_ID_DEV}',
-      measurementId: '${process.env.FIREBASE_MEASUREMENT_ID_DEV}',
-      projectId: '${process.env.FIREBASE_PROJECT_ID_DEV}',
-      storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET_DEV}'
-    }
-  };`;
 } else {
   targetPath += 'environment.ts';
-  envConfigFile = `export const environment = {
-  production: ${process.env.PRODUCTION},
-  environment: '${process.env.ENVIRONMENT}',
-  domain: '${process.env.DOMAIN_DEV}',
-  googleMapsApiKey: '${process.env.GOOGLE_MAPS_API_KEY}',
-  app: {
-    modules: {
-      ownerReport: true,
-      mapMenu: true,
-      mapFilter: true,
-      mapSearch: true
-    }
-  },
-  firebase: {
-    apiKey: '${process.env.FIREBASE_API_KEY_DEV}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN_DEV}',
-    databaseURL: '${process.env.FIREBASE_DATABASE_URL_DEV}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID_DEV}',
-    appId: '${process.env.FIREBASE_APP_ID_DEV}',
-    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID_DEV}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID_DEV}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET_DEV}'
-  }
-};`;
 }
+envConfigFile = getEnvironment();
 
 console.log(colors.magenta('The file environment will be written with the following content: \n'));
 console.log(colors.grey(envConfigFile));
@@ -108,3 +37,56 @@ writeFile(
        console.log(colors.magenta(`Angular environment file generated correctly at ${targetPath} \n`));
    }
 });
+
+function getEnvironment() {
+  const param = getParams();
+  return  `export const environment = {
+  production: ${param.production},
+  environment: '${param.environment}',
+  domain: '${param.domain}',
+  googleMapsApiKey: '${param.googleMapsApiKey}',
+  app: {
+  modules: {
+  ownerReport: true,
+  mapMenu: true,
+  mapFilter: true,
+  mapSearch: true
+  }
+  },
+  firebase: {
+    apiKey: '${param.apiKey}',
+    authDomain: '${param.authDomain}',
+    databaseURL: '${param.databaseURL}',
+    messagingSenderId: '${param.messagingSenderId}',
+    appId: '${param.appId}',
+    measurementId: '${param.measurementId}',
+    projectId: '${param.projectId}',
+    storageBucket: '${param.storageBucket}'
+  }};
+
+  `;
+}
+
+function getParams() {
+  return {
+    production: process.env.PRODUCTION,
+    environment: process.env.ENVIRONMENT,
+    domain: (environment === 'prod' || environment === 'staging') ? process.env.DOMAIN_PROD : process.env.DOMAIN_DEV,
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    apiKey: (environment === 'prod' || environment === 'staging') ? process.env.FIREBASE_API_KEY : process.env.FIREBASE_API_KEY_DEV,
+    authDomain: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_AUTH_DOMAIN : process.env.FIREBASE_AUTH_DOMAIN_DEV,
+    databaseURL: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_DATABASE_URL : process.env.FIREBASE_DATABASE_URL_DEV,
+    messagingSenderId: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_MESSAGING_SENDER_ID : process.env.FIREBASE_MESSAGING_SENDER_ID_DEV,
+    appId: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_APP_ID : process.env.FIREBASE_APP_ID_DEV,
+    measurementId: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_MEASUREMENT_ID : process.env.FIREBASE_MEASUREMENT_ID_DEV,
+    projectId: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_PROJECT_ID : process.env.FIREBASE_PROJECT_ID_DEV,
+    storageBucket: (environment === 'prod' || environment === 'staging') ?
+      process.env.FIREBASE_STORAGE_BUCKET : process.env.FIREBASE_STORAGE_BUCKET_DEV
+  }
+}
