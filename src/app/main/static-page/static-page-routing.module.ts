5d1538fa5c60b52b1c43927a3942f24fcae09186
willy kouagnia <willkoua@gmail.com>
2020-12-08 22:41:20 +0100

    fix contacus footer link

--- src/app/main/static-page/static-page-routing.module.ts
+++ src/app/main/static-page/static-page-routing.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {FaqComponent} from './faq/faq.component';
-import {NotFoundComponent} from './not-found/not-found.component';
 
 
 const routes: Routes = [
