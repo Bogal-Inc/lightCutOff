f151a91081229b7708b31780dd6138c0d70a7b2c
willy kouagnia <willkoua@gmail.com>
2021-07-01 00:16:11 -0400

    update config

--- set-env.ts
+++ set-env.ts
@@ -9,7 +9,7 @@ const colors = require('colors');
 
 // read the command line arguments passed with yargs
 const environment = argv.environment;
-const isProduction = environment === 'production';
+const isProduction = environment === 'prod';
 const environmentPath = isProduction
   ? `./src/environments/environment.prod.ts`
   : `./src/environments/environment.ts`;
